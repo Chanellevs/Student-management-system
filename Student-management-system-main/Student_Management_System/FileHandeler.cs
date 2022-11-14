04217dadced9782f273e815c23a0560066253bf9
Chanellevs <111304251+Chanellevs@users.noreply.github.com>
2022-11-14 12:19:37 +0200

    form main update

---
+++ Student-management-system-main/Student_Management_System/FileHandeler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_System
+{
+    internal class FileHandeler
+    {
+        private string filePath = "D:\\Student_Project\\Student-management-system";
+        List<AdminData> usersList = new List<AdminData>();
+        public void fileWrite(string userName, string password)
+        {
+            try
+            {
+                //Creating File path 
+                FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+
+                //Validation of streamwriter
+                if (!File.Exists(filePath))
+                {
+                    using (TextWriter riter = File.CreateText(filePath))
+                    {
+                        riter.WriteLine(userName + "," + password);
+                        Console.WriteLine("File Successfully written too");
+                    }
+                }
+                else if (File.Exists(filePath))
+                {
+                    using (TextWriter riter = File.AppendText(filePath))
+                    {
+                        riter.WriteLine(userName + "," + password);
+                        Console.WriteLine("File Successfully Written too");
+                        riter.Close();
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("File Write Not SuccessFull");
+            }
+        }
+
+        public List<AdminData> fileRead()
+        {
+            try
+            {
+                //Creating File path 
+                FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read);
+                using (StreamReader readStream = new StreamReader(fileStream))
+                {
+                    string line = readStream.ReadLine();
+                    string[] userArr;
+
+                    while (line != null)
+                    {
+                        userArr = line.Split(',');
+                        usersList.Add(new AdminData(userArr[0], userArr[1]));
+                        Console.WriteLine(userArr);
+                        line = readStream.ReadLine();
+                    }
+                    readStream.Close();
+                    return usersList;
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                return usersList;
+            }
+        }
+    }
+}
+
---
+++ Student-management-system-main/Student_Management_System/FormMain.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Student_Management_System
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(243, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(785, 209);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(243, 356);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(785, 202);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Module";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add Student";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Update Student";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(35, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 46);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete Student";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 46);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Display Student";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(35, 512);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 46);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Display Module";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(35, 460);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 46);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Delete Module";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(35, 408);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 46);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Update Module";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(35, 356);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(136, 46);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Add Module";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(892, 581);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 46);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Exit";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(292, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(467, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Belgium Campus student management ";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 636);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+    }
+}
