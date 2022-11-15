4268b5ad5261eef234a677ddaec687a205848398
J-KRoss <110404225+J-KRoss@users.noreply.github.com>
2022-11-15 14:02:09 +0200

    Another Update

--- Student_Management_System/Form1.cs
+++ Student_Management_System/Form1.cs
@@ -65,5 +65,13 @@ namespace Student_Management_System
                 }
             }
         }
+
+        private void btnCreateUser_Click(object sender, EventArgs e)
+        {
+            FileHandeler handler = new FileHandeler();
+            handler.fileWrite(username, password);
+            txtPassword.Clear();
+            txtUsername.Clear();
+        }
     }
 }
